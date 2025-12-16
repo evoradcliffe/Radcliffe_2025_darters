3f6387bd65b157fe939f14bf0f93c5dc0ea46ac2
evoradcliffe <wrad07@uchicago.edu>
2025-12-16 13:10:53 -0600

    Update run_fst.sh

--- popgen/run_fst.sh
+++ popgen/run_fst.sh
@@ -1,3 +1,21 @@
 module load GCC/13.2.0 VCFtools/0.1.16
 
-vcftools --gzvcf ../merged_RB2RB_ChrOnly_filtered_RepsRem_Indels10bpRem_nohybrids.vcf.gz --weir-fst-pop male_sym_IL.txt --weir-fst-pop female_sym_IL.txt --out sym_IL_mvf
+vcftools --vcf your_sympatric.vcf \
+  --weir-fst-pop male_samples.txt \
+  --weir-fst-pop female_samples.txt \
+  --out MvF_fst
+
+### Clean up data to remove any NaN, NA, etc.:
+awk '!/nan/ {print $1, $2, $3}' MvF_fst.weir.fst > MvF_fst_clean.txt
+
+### If you need to change scaffolds to their corresponding chromosomes (i.e., from the reference genome), run:
+awk 'NR==FNR { map[$1]=$2; next }
+     FNR==1 { print; next }
+     {
+       if ($1 in map) {
+         $1 = map[$1]
+       } else {
+         print "WARNING: No mapping for", $1 > "/dev/stderr"
+       }
+       print
+     }' scaffold_to_chromosome_map.txt OFS="\t" mvf.weir_clean.fst > mvf.chrom_fst
